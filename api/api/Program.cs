c8b8ddd719353db6e01dc0d40b80b9d057b8f7d0
Cesar Pereiro <cesar.pereiro@coremain.com>
2021-06-30 09:59:32 +0200

    prueba

---
+++ api/api/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+
+
+namespace api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getUser();
+        }
+        public static void getUser() {
+            var client = new RestClient("https://dummyapi.io/data/api/");
+            var request = new RestRequest("user");
+            request.AddHeader("Authorization", "Basic Z3JvdXAxOlByb2otMzI1");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("app-id", "60dc16d63b525f3ffed0de31");
+            var response = client.Execute(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK) {
+                dynamic resp = JsonConvert.DeserializeObject(response.Content);
+                Console.Out.WriteLine(resp.data[0].id);
+            }
+        }
+    }
+}
